17e22e5bfd09a58cb587bf066f6fac5095443208
Namae Conde <ATBBXZ8ZW3RMtSZD6rEEUrh6L3L58F3A2201>
2023-05-18 11:27:20 +1000

    Update text visibility

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,17 +40,17 @@ export default function Home() {
                 <span className="sr-only">Loading...</span>
             </div> :
           <div
-              className="mb-32 grid text-center place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+              className="mb-32 grid text-center place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
             <h2 className={`mb-3 text-2xl font-bold`}>
               TODAY&apos;S READING
             </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-70`}>
+            <p className={`text-sm`}>
               Date: {readingToday["Date"]}
             </p>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            <p className={`text-sm`}>
               Old Testament: {readingToday["Old Testament"]}
             </p>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            <p className={`text-sm`}>
               New Testament: {readingToday["New Testament"]}
             </p>
             <a
