4fc3e32665550aec74926d074b55e5fd5fd61905
Namae Conde <ATBBXZ8ZW3RMtSZD6rEEUrh6L3L58F3A2201>
2023-05-18 11:53:41 +1000

    Update text to center xy axis

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,18 +29,25 @@ export default function Home() {
 
   return (
       <main
-          className={`flex min-h-screen flex-col items-center justify-between p-24 ${robotoMono.className}`}
+          className={`flex min-h-screen py-24 ${robotoMono.className}`}
       >
         { isLoading ?
-            <div role="status" className="max-w-sm animate-pulse">
-                <div className="h-5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-                <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
-                <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-                <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-                <span className="sr-only">Loading...</span>
-            </div> :
+          <div role="status" className="max-w-sm animate-pulse">
+            <div className="h-5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+            <span className="sr-only">Loading...</span>
+          </div> :
           <div
-              className="mb-32 grid text-center place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+              className="grid m-auto text-center place-items-center
+              before:rounded-full before:bg-gradient-radial
+              before:blur-2xl before:content-[''] after:absolute after:-z-20
+              after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic
+              after:from-sky-200 after:via-blue-200 after:blur-2xl
+              after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent
+              before:dark:to-blue-700/10 after:dark:from-sky-900
+              after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
             <h2 className={`mb-3 text-2xl font-bold`}>
               TODAY&apos;S READING
             </h2>
