f81d8b05980e17d88c20abfabae179bff011ef10
Namae Conde <ATBBXZ8ZW3RMtSZD6rEEUrh6L3L58F3A2201>
2023-05-20 01:04:55 +1000

    Update text opacity and loading indicator

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,43 +33,41 @@ export default function Home() {
       >
         { isLoading ?
           <div role="status" className="max-w-sm m-auto animate-pulse">
-            <div className="h-5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
-            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-            <span className="sr-only">Loading...</span>
+            <span className="relative flex h-3 w-3">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+            </span>
           </div> :
           <div
-              className="grid m-auto text-center place-items-center
-              before:rounded-full before:bg-gradient-radial
-              before:blur-2xl before:content-[''] after:absolute after:-z-20
-              after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic
-              after:from-sky-200 after:via-blue-200 after:blur-2xl
-              after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent
-              before:dark:to-blue-700/10 after:dark:from-sky-900
-              after:dark:via-[#0141ff]/40">
-            <h2 className={`mb-3 text-2xl font-bold`}>
+            className="grid m-auto text-center place-items-center
+            before:rounded-full before:bg-gradient-radial
+            before:blur-2xl before:content-[''] after:absolute after:-z-20
+            after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic
+            after:from-sky-200 after:via-blue-200 after:blur-2xl
+            after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent
+            before:dark:to-blue-700/10 after:dark:from-sky-900
+            after:dark:via-[#0141ff]/40">
+            <h2 className={`mb-3 text-2xl font-bold opacity-70`}>
               TODAY&apos;S READING
             </h2>
-            <p className={`text-sm`}>
+            <p className={`text-sm opacity-70`}>
               Date: {readingToday["Date"]}
             </p>
-            <p className={`text-sm`}>
+            <p className={`text-sm opacity-70`}>
               Old Testament: {readingToday["Old Testament"]}
             </p>
-            <p className={`text-sm`}>
+            <p className={`text-sm opacity-70`}>
               New Testament: {readingToday["New Testament"]}
             </p>
-            <a
-                href={`https://www.biblegateway.com/passage/?search=${readingToday["Old Testament"]},${readingToday["New Testament"]}&version=${VERSION}`}
-                className="group rounded-lg border border-transparent p-2 m-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              <h2 className={`text-xl font-semibold`}>
+            <a href={`https://www.biblegateway.com/passage/?search=${readingToday["Old Testament"]},${readingToday["New Testament"]}&version=${VERSION}`}
+              className="group rounded-lg border border-transparent p-2 m-2
+               transition-colors border-gray-300 bg-gray-100
+               dark:border-neutral-700 dark:bg-neutral-800/30"
+              target="_self"
+              rel="noopener noreferrer">
+              <h2 className={`text-xl font-semibold opacity-70`}>
                 Go{' '}
-                <span
-                    className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
               </h2>
